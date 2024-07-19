ef35d721d5abbe024f41ec6fd0b0656c897a79b9
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-19 11:34:33 -0400

    js03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let outputString = '';
+    for(let i=1 ; i<= string.length; i++){
+        outputString += string[string.length - i ];
+    }
+    return outputString;
 };
 
 // Do not edit below this line
