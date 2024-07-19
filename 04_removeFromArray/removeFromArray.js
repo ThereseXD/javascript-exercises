574db0f394b21aa1b72c119d01e90975637c31f9
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-19 12:17:54 -0400

    JS04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr,...args) {
+//     const outputArr = [];
+//     for(let item of arr){
+//         if (!args.includes(item)){
+//             outputArr.push(item)
+//         }
+//     }
+//     return outputArr;
+// };
+
+const removeFromArray = function(arr,...args) {
+    const outputArr = arr.filter((item)=> !args.includes(item))
+    return outputArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
