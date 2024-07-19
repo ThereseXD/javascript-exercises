ab156cd98cd8ed590d35da79f223abab4266e06e
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-19 14:42:27 -0400

    JS07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFah) {
+   let tempInCel = (tempInFah - 32) / (9/5);
+   tempInCel = Math.round(tempInCel*10)/10;
+   return tempInCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  let tempInFah = tempInCel * (9/5) + 32;
+  tempInFah = Math.round(tempInFah*10)/10
+  return tempInFah;
 };
 
 // Do not edit below this line
