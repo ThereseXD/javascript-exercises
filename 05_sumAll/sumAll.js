1dea7191a8cd22f85aa267e7f02efc39ad697345
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-19 12:35:46 -0400

    js05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+   if (num1 <0 || num2 <0 || 
+    !Number.isInteger(num1) || !Number.isInteger(num2)){ 
+        return 'ERROR'
+    }
+
+    let minNum = 0;
+    let maxNum = 0;
+    
+    num1 > num2? 
+    (maxNum = num1, minNum = num2) :
+    (maxNum = num2, minNum = num1);
+
+    let outcomeSum = 0;
+
+    for (let i = minNum; i<= maxNum ; i++){
+        outcomeSum += i;
+    }
+
+    return outcomeSum;
 
 };
 
