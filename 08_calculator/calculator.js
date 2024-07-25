bae70a402af17beb4219b7f813b95a12ce825dc5
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-25 10:13:57 -0400

    JS08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,31 @@
-const add = function() {
+const add = function(...args) {
+	return args.reduce((total,num)=>total+=num, 0)
+};
+
+const subtract = function(...args) {
+	return Math.max(...args) - Math.min(...args);
+
+};
+
+const sum = function(array) {
+	return array.reduce((total,num)=>total+ num, 0)
+};
+
+const multiply = function(array) {
+  return array.reduce((total,num)=>total*=num, 1)
+};
+
+const power = function(num1,num2) {
+   return num1 ** num2;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  let output = 1;
+  for (let i=num; i > 0; i--){
+    output *= i;
+  }
+  return output;
 	
 };
 
