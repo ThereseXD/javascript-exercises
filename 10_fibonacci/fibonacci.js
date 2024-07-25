40cf9becb61869dc5de742db599e4c40c67285d4
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-25 12:52:35 -0400

    JS10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fibonacciArr = [0,1];
+    let index = parseInt(num)
+    if (index<0){
+        return 'OOPS'
+    } else if (fibonacciArr[index] === undefined){
+        for(let i=2; i <= index ; i++){
+            fibonacciArr[i] = fibonacciArr[i-1]+fibonacciArr[i-2];
+        }
+    } 
+    
+    return fibonacciArr[index];
 };
+
+console.log(fibonacci('1'))
+
 
 // Do not edit below this line
 module.exports = fibonacci;
