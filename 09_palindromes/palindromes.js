efa0ad59a368b758d835bcdcbc2a57cee9a26b75
ThereseXD <35051370+ThereseXD@users.noreply.github.com>
2024-07-25 12:13:59 -0400

    JS09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let lowerStr = str.toLowerCase();
+    console.log(lowerStr);
+
+    const arr = lowerStr.split('');
+    console.log(arr);
+
+    const filteredArr = arr.filter((letter) => {
+        let charCode = letter.charCodeAt(0)
+
+        return (charCode >= 48 && charCode <= 57) || (charCode >= 97 && charCode <= 122)
+    })
+
+    console.log(filteredArr)
+
+    let compare1 = filteredArr.join('');
+    let compare2 = filteredArr.reverse().join('')
+    
+
+    return compare1 === compare2
+
 
 };
+
+palindromes('Animal loots foliated detail of stool lamina.')
+palindromes('A car, a man, a maraca.')
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
